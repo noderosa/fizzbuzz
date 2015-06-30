815b529c8e46e9c5fb23a063525f897ca41b1009
noderosa <chie.miyajima@gmail.com>
2015-06-29 22:47:32 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
-for (var i = 1; i < 101; i++) {
-    if (i % 3 === 0 && i % 5 == 0) {
-    console.log("FizzBuzz");
+$(document).ready(function () {
+
+    for (var i = 1; i < 101; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            $('body').append("FizzBuzz <br>");
+        }
+        else if (i % 3 === 0) {
+            $('body').append("Fizz <br>");
+        }
+        else if (i % 5 === 0) {
+            $('body').append("Buzz <br>");
+        }
+        else {
+            $('body').append(i+"<br>");
+        }
     }
-    else if (i % 3 === 0) {
-    console.log("Fizz");
-    }
-    else if (i % 5 == 0) {
-    console.log("Buzz");
-    }
-    else {
-    console.log(i);
-    }
-    }
+
+});
